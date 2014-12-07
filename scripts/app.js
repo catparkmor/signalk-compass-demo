440368685ee84e1c80767f19ed00c9304e9941ba
Fabian Tollenaar <fabian@starting-point.nl>
2014-12-07 23:40:42 +0100

    added pitch & roll

--- scripts/app.js
+++ scripts/app.js
@@ -6,24 +6,29 @@
     , light
     , offset  = -0.09
     , last    = 0
-    , degrees = 0
+    , heading = 0
+    , pitch   = 0
+    , roll    = 0
     , vessel  = null
   ;
 
   var primus = Primus.connect('http://localhost:3000/signalk/stream');
 
   primus.on('data', function(data) {
-    deriveDegrees(data);
-    $('#value').html(degrees);
+    deriveData(data);
+
+    $('#heading').html(heading);
+    $('#pitch').html(pitch);
+    $('#roll').html(roll);
   });
 
-  function deriveDegrees(data) {
+  function deriveData(data) {
     if(vessel === null) {
       for(var id in data.vessels) {
         var ves = data.vessels[id];
 
         if(ves.navigation !== null && typeof ves.navigation === 'object') {
-          if(typeof ves.navigation.courseOverGroundMagnetic !== 'undefined') {
+          if(typeof ves.navigation.courseOverGroundTrue !== 'undefined') {
             vessel = id;
           }
         }
@@ -32,30 +37,51 @@
 
     data = data.vessels[vessel];
 
-    if(data.navigation !== null && typeof data.navigation === 'object' && data.navigation.courseOverGroundMagnetic !== null && typeof data.navigation.courseOverGroundMagnetic === 'object') {
-      degrees = data.navigation.courseOverGroundMagnetic.value;
+    if(data.navigation !== null && typeof data.navigation === 'object') {
+      if(data.navigation.courseOverGroundTrue !== null && typeof data.navigation.courseOverGroundTrue === 'object') {
+        heading = data.navigation.courseOverGroundTrue.value;
+      }
+
+      if(data.navigation.pitch !== null && typeof data.navigation.pitch === 'object') {
+        pitch = data.navigation.pitch.value;
+      }
+
+      if(data.navigation.roll !== null && typeof data.navigation.roll === 'object') {
+        roll = data.navigation.roll.value;
+      }
     }
   }
 
   function createSphere() {
-    var geometry  = new THREE.SphereGeometry(2.5, 50, 50);
+    var geometry    = new THREE.SphereGeometry(2, 50, 50);
     //var material  = new THREE.MeshBasicMaterial({ color: 0x00ffff, wireframe: true });
-    var material  = new THREE.MeshPhongMaterial();
-    material.map  = THREE.ImageUtils.loadTexture('styles/compass.png');
-    
-    sphere = new THREE.Mesh(geometry, material);
+    var material    = new THREE.MeshPhongMaterial();
+    material.map    = THREE.ImageUtils.loadTexture('styles/compass.png');
+    sphere          = new THREE.Mesh(geometry, material);
+
     scene.add(sphere);
   }
 
-  function addMainNeedle() {
-    var geometry = new THREE.Geometry();
-    var material = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 10 });
-    var mesh = new THREE.Line(geometry, material);
+  function addMainNeedle(clr) {
+    var geometry  = new THREE.Geometry();
+    var material  = new THREE.LineBasicMaterial({ color: clr, linewidth: 4 });
+    var mesh      = new THREE.Line(geometry, material);
 
-    geometry.vertices.push(new THREE.Vector3(0, -0.39, 3));
-    geometry.vertices.push(new THREE.Vector3(0, 0.39, 3));
+    geometry.vertices.push(new THREE.Vector3(0, -0.29, 3));
+    geometry.vertices.push(new THREE.Vector3(0, 0.29, 3));
 
     scene.add(mesh);
+  }
+
+  function addRing() {
+    var geometry    = new THREE.TorusGeometry(2.41, 0.2, 50, 50);
+    //var material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false });
+    var material    = new THREE.MeshPhongMaterial();
+    material.map    = THREE.ImageUtils.loadTexture('styles/quarters.png');
+   
+    var torus       = new THREE.Mesh(geometry, material);
+    
+    scene.add(torus);
   }
 
   function init() {
@@ -66,17 +92,36 @@
 
     scene.add(light);
     camera.position.z = 5;
+    
     renderer.setSize($(window).width(), $(window).height());
+    renderer.setClearColor(0x003300, 1);
+
     $('main').empty().append(renderer.domElement);
     
     createSphere();
-    addMainNeedle();
+    addMainNeedle(0x00CC00);
+    addRing();
     animate();
   }
 
   function calculateRotation() {
-    var deg = -(degrees - 90);
-    sphere.rotation.y = (deg * Math.PI / 180) + offset;
+    if(heading !== last) {
+      //* Heading
+      var compass = -(heading - 90);
+      sphere.rotation.y = (compass * Math.PI / 180) + offset;
+      //*/
+
+      //* Roll
+      sphere.rotation.x = (roll * Math.PI / 180);
+      sphere.rotation.z = (roll * Math.PI / 180);
+      //*/
+
+      //* Pitch
+      sphere.rotation.x = (pitch * Math.PI / 180);
+      //*/
+
+      last = heading;
+    }
   }
 
   function render() {
